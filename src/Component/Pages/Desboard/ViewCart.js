bfc9134d66b90cc16f1358f6dcc28ba671f5ba06
md-rakib420 <rayhan.senbag.bd@gmail.com>
2021-11-26 18:13:06 +0600

    count added

--- src/Component/Pages/Desboard/ViewCart.js
+++ src/Component/Pages/Desboard/ViewCart.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import useAuth from '../../Hook/useAuth';
 
 const ViewCart = () => {
     const [products, setProduct] = useState([]);
     const [cartProducts, setCartProducts] = useState([]);
-    const [quantity, setQuantity] = useState(1);
     const [isLoading, setIsLoading] = useState(true);
     const { addedProduct, setAddedProduct, user } = useAuth();
 
@@ -21,17 +21,31 @@ const ViewCart = () => {
             const newCartProducts = [];
             for (const cart of addedProduct) {
                 const findCartProduct = products.find(product => product._id === cart.id);
+                findCartProduct.quantity = cart.quantity;
                 newCartProducts.push(findCartProduct);
             }
             setCartProducts(newCartProducts);
             setIsLoading(false);
         }
-    }, [addedProduct, products,])
+    }, [addedProduct, products]);
 
-    const handleMinus = () => {
-        if (quantity > 1) {
-            setQuantity(quantity - 1)
+    const handlePlusMinus = (id, action) => {
+        const newCart = [];
+        for (const product of cartProducts) {
+            if (product._id === id) {
+                if (action === "minus" && product.quantity >= 2) {
+                    product.quantity -= 1;
+                }
+                if (action === "plus") {
+                    product.quantity += 1;
+                }
+                newCart.push(product);
+            }
+            else {
+                newCart.push(product);
+            }
         }
+        setCartProducts(newCart);
     }
 
     const handleDelete = (id) => {
@@ -64,12 +78,12 @@ const ViewCart = () => {
         <>
             {!cartProducts.length &&
                 <div className="text-center text-3xl py-8 text-gray-500">
-                    <h1>There no product you added to cart</h1>
+                    <h1>There no product you added</h1>
                 </div>
 
             }
-            {cartProducts.length && <div className="m-5 bg-white rounded">
-                {cartProducts &&
+            {cartProducts.length && <div className="m-5 bg-white rounded text-xl">
+                {
                     cartProducts.map(product => {
                         totalPrice += parseInt(product.price);
                         return <div
@@ -77,15 +91,16 @@ const ViewCart = () => {
                             className="p-3 grid grid-cols-4 justify-center items-center text-center">
                             <img src={product.img} alt="" />
                             <p>{product.name}</p>
-                            <p>{product.price}</p>
+                            <p>{product.price} BDT</p>
                             <div className="flex justify-evenly">
                                 <div className="flex items-center">
                                     <button
-                                        onClick={handleMinus} className="button">-
+                                        onClick={() => { handlePlusMinus(product._id, "minus") }} className="button">-
                                     </button>
-                                    <span>1</span>
+                                    <span>{product.quantity}</span>
                                     <button
-                                        onClick={() => { setQuantity(quantity + 1) }} className="button">+
+                                        onClick={() => { handlePlusMinus(product._id, "plus") }}
+                                        className="button">+
                                     </button>
                                 </div>
                                 <button
@@ -97,11 +112,15 @@ const ViewCart = () => {
                     })
                 }
                 {cartProducts.length &&
-                    <div className="grid grid-cols-4 text-center">
+                    <div className="grid grid-cols-4 text-center items-center">
                         <p></p>
                         <p></p>
-                        <p className="py-3">Total: {totalPrice}</p>
-                        <p></p>
+                        <p className="text-2xl">Total: <span className="font-semibold text-green-500">{totalPrice} BDT</span></p>
+                        <div className="flex justify-center">
+                            <Link to="">
+                                <button className="button">Pleace Order</button>
+                            </Link>
+                        </div>
                     </div>
                 }
             </div>}
