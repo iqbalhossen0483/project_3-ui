fe2467e20de66707edff94e2ed5b4f137bc9e7dc
md-rakib420 <rayhan.senbag.bd@gmail.com>
2021-12-03 12:54:06 +0600

    sssdkjf

--- src/Component/Pages/Home/Home.js
+++ src/Component/Pages/Home/Home.js
@@ -80,6 +80,7 @@ const Home = () => {
             <div className="my-16 md:px-5">
                 <h3 className={SectionHeader}>Our Customer Reviews</h3>
                 <div className="md:grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+
                     {
                         reviews.map(review => <Reviews key={review._id} review={review} />)
                     }
