c8076fe3ccdd1a445d931b8b15f3ffa199283dc3
md-rakib420 <rayhan.senbag.bd@gmail.com>
2021-12-15 00:00:53 +0600

    dd

--- src/Component/Pages/Desboard/Desboard.js
+++ src/Component/Pages/Desboard/Desboard.js
@@ -1,16 +1,43 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import useAuth from '../../Hook/useAuth';
 import Footer from '../../ShareComponent/Footer/Footer';
 import useTailwind from '../../TailwindCss/useTailwind';
 
 const Desboard = () => {
+    const [dsMenu, setDsMenu] = useState(true);
     const { link } = useTailwind();
     const { isAdmin } = useAuth();
+
+    const handleDsMenu = () => {
+        if (dsMenu) {
+            setDsMenu(false);
+        } else {
+            setDsMenu(true);
+        }
+    }
+    useEffect(() => {
+        if (window.innerWidth < 480) {
+            setDsMenu(false);
+        } else {
+            setDsMenu(true);
+        }
+    }, []);
+
     return (
         <div className="md:grid grid-cols-6">
+            <i
+                onClick={handleDsMenu}
+                className={`${dsMenu && "hidden"} md:hidden text-2xl text-green-500 fas fa-caret-square-right`}>
+            </i>
             <div style={{ width: "14.1rem" }}
-                className="flex flex-col bg-green-500 pt-5 h-full fixed">
+                className={`${!dsMenu && "hidden"} flex flex-col bg-green-500 pt-5 h-full fixed z-20`}>
+                <i
+                    onClick={handleDsMenu}
+                    className={`${!dsMenu && "hidden"} text-2xl md:hidden fas fa-times absolute top-3 right-3`}>
+                </i>
                 {
                     isAdmin && <>
                         <NavLink className={link} to='add-product'>Add-Product</NavLink>
